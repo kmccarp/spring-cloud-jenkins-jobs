d897bb9716b9b17aef05c9a79ed9e35c40b7258a
Ryan Baxter <rbaxter@vmware.com>
2022-06-08 15:22:43 -0400

    Updating kubernetes job for changes in building main

--- src/main/groovy/org/springframework/jenkins/cloud/common/KubernetesProject.groovy
+++ src/main/groovy/org/springframework/jenkins/cloud/common/KubernetesProject.groovy
@@ -8,20 +8,32 @@ import org.springframework.jenkins.common.job.JdkConfig
 class KubernetesProject extends Project implements JdkConfig, SpringCloudJobs {
 	@Override
 	List<String> customBuildCommand(BuildContext context) {
-		List<String> cmd = [
-				"./mvnw clean install -Pspring -B -U",
-				"""#!/bin/bash
+		if(context.branch == "2.0.x" || context.branch == "2.1.x") {
+			List<String> cmd = [
+					"./mvnw clean install -Pspring -B -U",
+					"""#!/bin/bash
 	cd spring-cloud-kubernetes-integration-tests
     ./run.sh
 """,
-				context.upload ? "./mvnw deploy -Pdocs,deploy,spring -B -U -DskipTests=true" : "./mvnw install -Pdeploy,spring -B -U -DskipTests=true",
-				"""./mvnw dockerfile:push -pl :spring-cloud-kubernetes-configuration-watcher -Pdockerpush""",
-		]
-		if (context.branch != "2.0.x") {
-			cmd.add "./mvnw dockerfile:push -pl :spring-cloud-kubernetes-configserver -Pdockerpush"
-			cmd.add "./mvnw dockerfile:push -pl :spring-cloud-kubernetes-discoveryserver -Pdockerpush"
+					context.upload ? "./mvnw deploy -Pdocs,deploy,spring -B -U -DskipTests=true" : "./mvnw install -Pdeploy,spring -B -U -DskipTests=true",
+					"""./mvnw dockerfile:push -pl :spring-cloud-kubernetes-configuration-watcher -Pdockerpush""",
+			]
+			if (context.branch != "2.0.x") {
+				cmd.add "./mvnw dockerfile:push -pl :spring-cloud-kubernetes-configserver -Pdockerpush"
+				cmd.add "./mvnw dockerfile:push -pl :spring-cloud-kubernetes-discoveryserver -Pdockerpush"
+			}
+			return cmd
 		}
-		return cmd
+		else {
+			List<String> cmd = [
+					"./mvnw clean install -Pspring -B -U",
+					context.upload ? "./mvnw deploy -Pdocs,deploy,spring -B -U -DskipTests=true" : "./mvnw install -Pdeploy,spring -B -U -DskipTests=true",
+					"""./mvnw dockerfile:push -pl :spring-cloud-kubernetes-configuration-watcher -Pdockerpush""",
+					"./mvnw dockerfile:push -pl :spring-cloud-kubernetes-configserver -Pdockerpush",
+					"./mvnw dockerfile:push -pl :spring-cloud-kubernetes-discoveryserver -Pdockerpush"
+			]
+			return cmd
+		}
 	}
 
 }
